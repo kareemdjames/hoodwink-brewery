f6c3d1c20373362377b231242502cece66931a4e
Kareem James <kareem.james@detroitlabs.com>
2020-07-08 08:46:10 -0400

    added delete mapping for beer

--- src/main/java/com/hoodwink/hoodwinkbrewery/services/BeerService.java
+++ src/main/java/com/hoodwink/hoodwinkbrewery/services/BeerService.java
@@ -13,4 +13,6 @@ public interface BeerService {
     BeerDto saveNewBeer(BeerDto beerDto);
 
     void updateBeer(UUID beerId, BeerDto beerDto);
+
+    void deletebyId(UUID beerId);
 }
--- src/main/java/com/hoodwink/hoodwinkbrewery/services/BeerServiceImpl.java
+++ src/main/java/com/hoodwink/hoodwinkbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.hoodwink.hoodwinkbrewery.services;
 
 import com.hoodwink.hoodwinkbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
 
+    }
+
+    @Override
+    public void deletebyId(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
