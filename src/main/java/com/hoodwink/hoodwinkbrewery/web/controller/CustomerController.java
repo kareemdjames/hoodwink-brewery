9c313eecea1837c1d13ea84ea3cb7bb395c842d3
Kareem James <kareemdjames@gmail.com>
2020-07-22 19:13:48 -0400

    added controller advice for controller level exception handling

--- src/main/java/com/hoodwink/hoodwinkbrewery/web/controller/CustomerController.java
+++ src/main/java/com/hoodwink/hoodwinkbrewery/web/controller/CustomerController.java
@@ -52,15 +52,4 @@ public class CustomerController {
 
     }
 
-    @ExceptionHandler(ConstraintDeclarationException .class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-
 }
